3b15fb563250032cbb743372d22c9e16f9b2a5ae
Julian Schmidt <contact@julianschmidt.codes>
2024-12-24 00:55:41 +0100

    part 2

--- 2024/05/main.go
+++ 2024/05/main.go
@@ -6,9 +6,47 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
+
+func convertStringToArray(s string) []string {
+	parts := strings.Split(s, ",")
+	return parts
+}
+
+func fulfillRule(rule string, update string) string {
+	// rules are in the format "\d+|\d+"
+	// regex := regexp.MustCompile(rule)
+	// matches := regex.FindAllString(update, -1)
+
+	uA := convertStringToArray(update)
+
+	parts := strings.Split(rule, "|")
+	first, second := parts[0], parts[1]
+
+	firstIndex := slices.Index(uA, first)
+	secondIndex := slices.Index(uA, second)
+
+	// fmt.Println("Found", first, "at", firstIndex)
+	// fmt.Println("Found", second, "at", secondIndex)
+
+	// now move firstIndex to left of secondIndex
+	// e.g. ... , first, second, ...
+
+	// insert update[firstIndex] at secondIndex
+	newArray := slices.Delete(uA, firstIndex, firstIndex+1)
+	newArray = slices.Insert(newArray, secondIndex, first)
+
+	// newArray := append(uA[:secondIndex], uA[firstIndex], uA[secondIndex])
+	// newArray = append(newArray, uA[firstIndex+1:]...)
+
+	// fmt.Println("new array length:", len(newArray))
+
+	return strings.Join(newArray, ",")
+}
 
 func isUpdateAllowed(update string) bool {
 	for _, rule := range rules {
@@ -39,16 +77,50 @@ func isUpdateAllowed(update string) bool {
 		// fmt.Println("Found", second, "at", secondIndex)
 
 		if firstIndex > secondIndex {
-			fmt.Println("Rule", rule, "violated by", update)
 			return false
 		}
 	}
 	return true
 }
 
+func reorderUpdate(update string) string {
+	for _, rule := range rules {
+		parts := strings.Split(rule, "|")
+		first, second := parts[0], parts[1]
+
+		// fmt.Println(first, "must come before", second)
+
+		firstIndex := strings.Index(update, first)
+		secondIndex := strings.Index(update, second)
+
+		if firstIndex == -1 || secondIndex == -1 {
+			// this rule does not apply
+			continue
+		}
+
+		// fmt.Println("Found", first, "at", firstIndex)
+		// fmt.Println("Found", second, "at", secondIndex)
+
+		if firstIndex > secondIndex {
+			// fmt.Println("Rule", rule, "violated by", update)
+			newUpdate := fulfillRule(rule, update)
+			// fmt.Println("Update has been changed to", newUpdate)
+
+			if isUpdateAllowed(newUpdate) {
+				return newUpdate
+			} else {
+				return reorderUpdate(newUpdate)
+			}
+		}
+	}
+	return update
+}
+
 var rules = []string{}
 
 func main() {
+	startTime := time.Now()
+
 	file, err := os.Open("input1.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -81,7 +153,14 @@ func main() {
 			// fmt.Println(u, "is allowed")
 
 			// we have to find the middle number
-			parts := strings.Split(u, ",")
+			// parts := strings.Split(u, ",")
+			// middle, _ := strconv.Atoi(parts[len(parts)/2])
+
+			// sum += middle
+		} else {
+			newU := reorderUpdate(u)
+
+			parts := strings.Split(newU, ",")
 			middle, _ := strconv.Atoi(parts[len(parts)/2])
 
 			sum += middle
@@ -93,4 +172,7 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	endTime := time.Now()
+	fmt.Println("Execution Time:", endTime.Sub(startTime))
 }
