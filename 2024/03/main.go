0edddb3edd22a078810ddee02a39cf1ce6e4346a
Julian Schmidt <contact@julianschmidt.codes>
2024-12-23 22:10:38 +0100

    implement part 1

---
+++ 2024/03/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func compute(expression string) int {
+	// fmt.Println()
+	// fmt.Println(expression)
+
+	pattern := `(mul)\((\d{1,3}),(\d{1,3})\)`
+	re := regexp.MustCompile(pattern)
+
+	matches := re.FindAllStringSubmatch(expression, -1)[0]
+
+	if matches[1] == "mul" {
+		one, _ := strconv.Atoi(matches[2])
+		two, _ := strconv.Atoi(matches[3])
+
+		return one * two
+	}
+
+	return 0
+}
+
+func handleLine(line string) int64 {
+	// fmt.Println(line)
+
+	pattern := `mul\(\d{1,3},\d{1,3}\)`
+	re := regexp.MustCompile(pattern)
+
+	matches := re.FindAllString(line, -1)
+
+	sum := int64(0)
+	for _, match := range matches {
+		sum += int64(compute(match))
+	}
+
+	return sum
+}
+
+func main() {
+	file, err := os.Open("input1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	sum := int64(0)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		sum += handleLine(line)
+	}
+	fmt.Println("Sum:", sum)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
